423a195c741f39d0218ec14bdb1c20c9e1951ef0
mitsusaburo <bschindlerm@gmail.com>
2023-10-26 17:07:19 -0300

    Finish project

--- javascript.js
+++ javascript.js
@@ -16,13 +16,13 @@ function divide(x, y) {
 
 function operate(operator, num1, num2) {
     if (operator === "+") {
-        add(num1, num2);
+        return add(num1, num2);
     } else if (operator === "-") {
-        substract(num1, num2);
+        return substract(num1, num2);
     } else if (operator === "*") {
-        mult(num1, num2);
+        return mult(num1, num2);
     } else if (operator === "/") {
-        divide(num1, num2)
+        return divide(num1, num2)
     } 
 }
 
@@ -30,13 +30,42 @@ const screen = document.querySelector(".screen");
 const numberButtons = document.querySelectorAll(".num-button");
 const clear = document.querySelector(".clear");
 const backspace = document.querySelector(".backspace");
+const equals = document.querySelector(".equals");
 
-backspace.addEventListener("click", () => {
-    screen.value = screen.value.substring(0, screen.value.length-1);
+
+equals.addEventListener("click", () => {
+    const operation = screen.value;
+    const regex1 = /^\d+/g;
+    const regex2 = /\D/g;
+    const regex3 = /\d+$/g;
+    const x = operation.match(regex1)[0];
+    const operator = operation.match(regex2)[0];
+    const y = operation.match(regex3)[0];
+
+    screen.value = operate(operator, parseInt(x), parseInt(y));
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 clear.addEventListener("click", () => {
     screen.value = "";
+})
+
+backspace.addEventListener("click", () => {
+    screen.value = screen.value.substring(0, screen.value.length-1);
 })
 
 numberButtons.forEach((button) => {
